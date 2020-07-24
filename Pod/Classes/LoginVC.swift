02d636a23cb3ba1e80e43fb7cab2cdc2ff06c2db
Rahul Karanjit <rahul.karanjit@cellcom.net.np>
2020-07-24 12:07:30 +0545

    LoginVC has been added

---
+++ Pod/Classes/LoginVC.swift
@@ -0,0 +1,15 @@
+//
+//  LoginVC.swift
+//  MyFramework
+//
+//  Created by Cellcom on 7/24/20.
+//
+
+import Foundation
+import UIKit
+public class LoginVC: UIViewController{
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
