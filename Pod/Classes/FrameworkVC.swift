0517f4d42def8c88071960f6a4057047137ace42
Rahul Karanjit <rahul.karanjit@cellcom.net.np>
2020-07-24 11:55:23 +0545

    FrameworkVC has been changed

--- Pod/Classes/FrameworkVC.swift
+++ Pod/Classes/FrameworkVC.swift
@@ -16,18 +16,18 @@ public class FrameworkVC: UIViewController {
         let podBundle = Bundle(for: FrameworkVC.self)
         
         let bundleURL = podBundle.url(forResource: "MyFramework", withExtension: "bundle")
-        let bundle = Bundle(url: bundleURL!)!
-        let cellNib = UINib(nibName: "OurCell", bundle: bundle)
-        tableView.register(cellNib, forCellReuseIdentifier: "OurCell")
-        tableView.estimatedRowHeight = 80
-        tableView.rowHeight = UITableView.automaticDimension
+//        let bundle = Bundle(url: bundleURL!)!
+//        let cellNib = UINib(nibName: "OurCell", bundle: bundle)
+//        tableView.register(cellNib, forCellReuseIdentifier: "OurCell")
+//        tableView.estimatedRowHeight = 80
+//        tableView.rowHeight = UITableView.automaticDimension
     }
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                   return tableView.dequeueReusableCell(withIdentifier: "OurCell")!
-       }
-       
-       public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return 1
-       }
+//    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        return tableView.dequeueReusableCell(withIdentifier: "OurCell")!
+//       }
+//
+//       public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//           return 1
+//       }
 }
 
